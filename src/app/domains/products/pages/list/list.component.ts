bb7104cb153e3fe7b93a0d7949cc9aa9831c9aea
Carlos Perales <carlosperales050@gmail.com>
2024-02-14 19:49:15 -0600

    add: header

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [ProductComponent],
+  imports: [ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css',
 })
@@ -13,6 +14,27 @@ export class ListComponent {
   productList = signal<Product[]>([]);
   constructor() {
     const initalProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'producto 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=01',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'producto 2',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=02',
+        creationAt: new Date().toISOString(),
+      },
+      {
+        id: Date.now(),
+        title: 'producto 3',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=03',
+        creationAt: new Date().toISOString(),
+      },
       {
         id: Date.now(),
         title: 'producto 1',
